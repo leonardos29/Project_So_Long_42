9067fe2ad8a2b36152d7a55a31c989effcfabd8e
leonardos29 <leonardo.dev29@gmail.com>
2025-09-05 06:02:19 +0100

    add clear functions

--- src/handle_controls/handle_controls.c
+++ src/handle_controls/handle_controls.c
@@ -1,5 +1,25 @@
 #include "../../include/so_long.h"
 
+int verify_tile(t_game_data *data, char tile, int x, int y)
+{
+     if (tile == '1')
+        return(1);
+    if (tile == 'C')
+    {
+        data->collected++;
+        data->map[y][x] = '0';
+        if(data -> collected == data -> collect)
+          data->exit_img = mlx_xpm_file_to_image(data->mlx, "assets/exit_open.xpm",&data -> tile_w, &data -> tile_h);  
+    }
+    if (tile == 'E')
+    {
+        if (data->collected == data->collect)
+            clear_victory(data);
+        else
+            return(1);
+    }
+    return(0);
+}
 
 void	move_player(t_game_data *data, int dx, int dy)
 {
@@ -10,25 +30,8 @@ void	move_player(t_game_data *data, int dx, int dy)
     if (new_x < 0 || new_x >= data->map_w || new_y < 0 || new_y >= data->map_h)
         return;
     next_tile = data->map[new_y][new_x];
-    if (next_tile == '1')
+    if(verify_tile(data, next_tile,new_x,new_y))
         return;
-    if (next_tile == 'C')
-    {
-        data->collected++;
-        data->map[new_y][new_x] = '0';
-        if(data -> collected == data -> collect)
-          data->exit_img = mlx_xpm_file_to_image(data->mlx, "assets/exit_open.xpm",&data -> tile_w, &data -> tile_h);  
-    }
-    if (next_tile == 'E')
-    {
-        if (data->collected == data->collect)
-        {
-            ft_printf("Parabéns! Você venceu em %d movimentos!\n", data->moves + 1);
-            exit(0);
-        }
-        else
-            return; 
-    }
     data->map[data->player_y][data->player_x] = '0';
     data->map[new_y][new_x] = 'P';                  
     data->player_x = new_x;
@@ -42,7 +45,7 @@ int handle_key(int keycode, t_game_data *data)
 {
     if(keycode == 65307)
     {
-        //limpar tudo e fechar
+        clear_map(data -> map, data -> map_h);
         exit(0);
     }
     else if(keycode == 'w')
